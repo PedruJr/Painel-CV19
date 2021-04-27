f630bc8e51d5724fc6f5793c18680a4dab434892
Pedro Junior <you@example.com>
2021-04-27 20:25:18 -0300

    Map functions ready

--- frontend/src/components/map/Map.js
+++ frontend/src/components/map/Map.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import './map.scss';
+import ponte from '../../assets/mapa0.png';
 
 export default function Map() {
     const fetch = require('node-fetch');
@@ -9,9 +10,20 @@ export default function Map() {
     const filters = {state: "SC", is_last: "True"};
     const url=`https://api.brasil.io/v1/dataset/${datasetSlug}/${tableName}/data?state=${filters.state}&is_last=${filters.is_last}`;
 
-    const [biguacu, setBiguacu] = useState('');
+    const fetchData = [fetch,token,url];
+
+    const [biguaçu, setBiguaçu] = useState('');
+    const [palhoça, setPalhoça] = useState('');
+    const [saoJose, setSaoJose] = useState('');
+    const [floripa, setFloripa] = useState('');
+    const [currentCity, setCurrentCity] = useState('');
     
-    let data;
+    const data = {
+        biguaçu,
+        palhoça,
+        saoJose,
+        floripa
+    };
 
     useEffect(() => {
         fetch(
@@ -25,25 +37,70 @@ export default function Map() {
             ).then(res => res.json())
             .then(json => {
                 console.log(json);
-                console.log(json.results[38]);
+                setBiguaçu(json.results[38]);
+                console.log(biguaçu);
+                setFloripa(json.results[90]);
+                console.log(floripa);
+                setPalhoça(json.results[184]);
+                console.log(palhoça);
+                setSaoJose(json.results[251])
+                console.log(saoJose);
             });
-    },token,fetch,url) 
+    },fetchData) 
+
+    function setCity(value){
+
+        switch(value){
+            case "biguaçu":
+                setCurrentCity(biguaçu);
+                break;
+            case "floripa":
+                setCurrentCity(floripa);
+                break;
+            case "palhoça":
+                setCurrentCity(palhoça);
+                break;
+            case "saoJose":
+                setCurrentCity(saoJose);
+                break;
+        }
+
+    }
+
+
+
 
     return (
-        <div className="infosCard">
-            <div className="infosCard-text">
-                <h3>Cidade:</h3>
-                <p>Floripa</p>
-                <h3>Data</h3>
-                <p>30/03/3333</p>
-                <h3>População estimada:</h3>
-                <p>30.000</p>
-                <h3>Casos confirmados:</h3>
-                <p>300.000</p>
-                <h3>Taxa de mortalidade:</h3>
-                <p>300.000</p>
-                <h3>Ultimas mortes:</h3>
-                <p>300.000</p>
+        <div className="container">
+            <h1>Mapa Informativo</h1>
+            <hr></hr>
+            <div className="map-container">
+                <img className="map-img" src={ponte} alt="logo"/>
+                <button type="submit" id="biguaçu-btn" onClick={()=> setCity("biguaçu")}>Biguaçu</button>
+                <button type="submit" id="floripa-btn" onClick={()=> setCity("floripa")}>Florianopolis</button>
+                <button type="submit" id="palhoça-btn" onClick={()=> setCity("palhoça")}>Palhoça</button>
+                <button type="submit" id="saoJose-btn" onClick={()=> setCity("saoJose")}>São Jose</button>
+            </div>
+            <div className="infosCard">
+                <div className="infosCard-text">
+                    <h3>Cidade:</h3>
+                    <p>{currentCity.city}</p>
+                    <h3>Data</h3>
+                    <p>{currentCity.date}</p>
+                    <h3>População estimada:</h3>
+                    <p>{currentCity.estimated_population}</p>
+                    <h3>Casos confirmados:</h3>
+                    <p>{currentCity.last_available_confirmed}</p>
+                    <h3>Taxa de mortalidade:</h3>
+                    <p>{currentCity.last_available_death_rate}</p>
+                    <h3>Ultimas mortes:</h3>
+                    <p>{currentCity.last_available_deaths}</p>
+                    <h3>Novos confirmados:</h3>
+                    <p>{currentCity.new_confirmed}</p>
+                    <h3>Novas mortes:</h3>
+                    <p>{currentCity.new_deaths}</p>
+                    <button type="submit" value="biguaçu" onClick={()=> setCity("biguaçu")}>Change City</button>
+                </div>
             </div>
         </div>
     )
