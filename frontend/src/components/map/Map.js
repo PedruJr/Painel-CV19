404f8c00b310e45306d066ee6514548a194886ee
Pedro L A Junior <pedrotark@gmail.com>
2021-06-13 20:23:55 -0300

    Map hd add, hover 3d effects add

--- frontend/src/components/map/Map.js
+++ frontend/src/components/map/Map.js
@@ -13,8 +13,8 @@ export default function Map() {
 
     const fetchData = [fetch,token,url];
 
-    const [biguaçu, setBiguaçu] = useState('');
-    const [palhoça, setPalhoça] = useState('');
+    const [biguacu, setBiguacu] = useState('');
+    const [palhoca, setPalhoca] = useState('');
     const [saoJose, setSaoJose] = useState('');
     const [floripa, setFloripa] = useState('');
     const [currentCity, setCurrentCity] = useState('');
@@ -22,8 +22,9 @@ export default function Map() {
     const [showMap, setShowMap] = useState(false);
 
     const data = {
-        biguaçu,
-        palhoça,
+
+        biguacu,
+        palhoca,
         saoJose,
         floripa
     };
@@ -34,36 +35,36 @@ export default function Map() {
             {
                 method: 'get',
                 headers: {
-                Authorization: `Token ${token}`,
+                    Authorization: `Token ${token}`,
                 },
             }
-            ).then(res => res.json())
+        ).then(res => res.json())
             .then(json => {
                 console.log(json);
-                setBiguaçu(json.results[38]);
-                console.log(biguaçu);
+                setBiguacu(json.results[38]);
+                console.log(biguacu);
                 setFloripa(json.results[90]);
                 console.log(floripa);
-                setPalhoça(json.results[184]);
-                console.log(palhoça);
+                setPalhoca(json.results[184]);
+                console.log(palhoca);
                 setSaoJose(json.results[251])
                 console.log(saoJose);
             });
-    },fetchData) 
+    },fetchData)
 
     function setCity(value){
 
         switch(value){
-            case "biguaçu":
-                setCurrentCity(biguaçu);
+            case "biguacu":
+                setCurrentCity(biguacu);
                 setShow("infosCard-show");
                 break;
             case "floripa":
                 setCurrentCity(floripa);
                 setShow("infosCard-show");
                 break;
-            case "palhoça":
-                setCurrentCity(palhoça);
+            case "palhoca":
+                setCurrentCity(palhoca);
                 setShow("infosCard-show");
                 break;
             case "saoJose":
@@ -81,7 +82,7 @@ export default function Map() {
                 <div >
                     <h1>
                         <FiMap style={{marginRight:'0.3em'}}/>
-                         Mapa Informativo
+                        Mapa Informativo
                         <FiMap style={{marginLeft:'0.3em'}}/>
                     </h1>
                 </div>
@@ -96,30 +97,30 @@ export default function Map() {
                 <div className={`map-container ${showMap ? 'showMap' : ''}`}>
                     <img className="map-img" src={ponte} alt="logo"/>
 
-                    <button type="submit" id="biguaçu-btn" onClick={()=> setCity("biguaçu")}>Biguaçu</button>
+                    <button type="submit" id="biguacu-btn" onClick={()=> setCity("biguacu")}>Biguaçu</button>
                     <button type="submit" id="floripa-btn" onClick={()=> setCity("floripa")}>Florianopolis</button>
-                    <button type="submit" id="palhoça-btn" onClick={()=> setCity("palhoça")}>Palhoça</button>
+                    <button type="submit" id="palhoca-btn" onClick={()=> setCity("palhoca")}>Palhoça</button>
                     <button type="submit" id="saoJose-btn" onClick={()=> setCity("saoJose")}>São Jose</button>
 
-               <div className={`infosCard ${show}`}>
-                            <h3>Cidade:</h3>
-                            <p>{currentCity.city}</p>
-                            <h3>Data</h3>
-                            <p>{currentCity.date}</p>
-                            <h3>População estimada:</h3>
-                            <p>{currentCity.estimated_population}</p>
-                            <h3>Casos confirmados:</h3>
-                            <p>{currentCity.last_available_confirmed}</p>
-                            <h3>Taxa de mortalidade:</h3>
-                            <p>{currentCity.last_available_death_rate}</p>
-                            <h3>Ultimas mortes:</h3>
-                            <p>{currentCity.last_available_deaths}</p>
-                            <h3>Novos confirmados:</h3>
-                            <p>{currentCity.new_confirmed}</p>
-                            <h3>Novas mortes:</h3>
-                            <p>{currentCity.new_deaths}</p>
-                            <button type="submit" id="hidePainel-btn" onClick={()=> setShow(" ")}></button>
-                </div>
+                    <div className={`infosCard ${show}`}>
+                        <h3>Cidade:</h3>
+                        <p>{currentCity.city}</p>
+                        <h3>Data</h3>
+                        <p>{currentCity.date}</p>
+                        <h3>População estimada:</h3>
+                        <p>{currentCity.estimated_population}</p>
+                        <h3>Casos confirmados:</h3>
+                        <p>{currentCity.last_available_confirmed}</p>
+                        <h3>Taxa de mortalidade:</h3>
+                        <p>{currentCity.last_available_death_rate}</p>
+                        <h3>Ultimas mortes:</h3>
+                        <p>{currentCity.last_available_deaths}</p>
+                        <h3>Novos confirmados:</h3>
+                        <p>{currentCity.new_confirmed}</p>
+                        <h3>Novas mortes:</h3>
+                        <p>{currentCity.new_deaths}</p>
+                        <button type="submit" id="hidePainel-btn" onClick={()=> setShow(" ")}></button>
+                    </div>
                 </div>
             </div>
         </div>
